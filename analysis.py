073662420175e753c5f33cbcdd09ba95778daf95
Orion Bowers <orionbowers@gmail.com>
2024-03-09 12:42:26 -0700

    start analysis

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 # %%
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 # %%
 crime = pd.read_csv("crimes_cleaned.csv")
@@ -7,3 +8,9 @@ crime['Arrest'] = crime['Arrest'].astype(int)
 crime['Domestic'] = crime['Domestic'].astype(int)
 # %%
 crime.head()
+
+# %%
+X_train, X_test, y_train, y_test = train_test_split(crime.drop('Primary Type',axis=1), crime['Primary Type'],random_state=101, train_size=.8)
+
+# %%
+X_train
